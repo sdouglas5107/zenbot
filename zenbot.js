24f822f990a8c9b534d02d9c98451101f99a7d3b
Douglas Soares <douglas.soares@concrete.com.br>
2018-08-02 14:32:32 -0300

    Change port to bind

--- zenbot.js
+++ zenbot.js
@@ -1,4 +1,3 @@
-
 var semver = require('semver')
 var path = require('path')
 var program = require('commander')
@@ -11,34 +10,36 @@ if (semver.gt('8.3.0', versions.node)) {
   process.exit(1)
 }
 
-var fs = require('fs')
-  , boot = require('./boot')
+var fs = require('fs'),
+  boot = require('./boot')
 
-boot(function (err, zenbot) {
+boot(function(err, zenbot) {
   if (err) {
     throw err
   }
   program.version(zenbot.version)
 
   var command_directory = './commands'
-  fs.readdir(command_directory, function(err, files){
+  fs.readdir(command_directory, function(err, files) {
     if (err) {
       throw err
     }
-    
-    var commands = files.map((file)=>{
+
+    var commands = files.map((file) => {
       return path.join(command_directory, file)
-    }).filter((file)=>{
+    }).filter((file) => {
       return fs.statSync(file).isFile()
     })
 
-    commands.forEach((file)=>{
-      require(path.resolve(__dirname, file.replace('.js','')))(program, zenbot.conf)
+    commands.forEach((file) => {
+      require(path.resolve(__dirname, file.replace('.js', '')))(program, zenbot.conf)
     })
 
     program
-      .command('*', 'Display help', { noHelp: true })
-      .action((cmd)=>{
+      .command('*', 'Display help', {
+        noHelp: true
+      })
+      .action((cmd) => {
         console.log('Invalid command: ' + cmd)
         program.help()
       })
